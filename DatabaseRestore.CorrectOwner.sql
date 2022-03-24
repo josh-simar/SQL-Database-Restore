6deec682fdc8b430636baacde944ffd8f7c12e54
josh-simar <30680994+josh-simar@users.noreply.github.com>
2022-03-23 20:18:02 -0700

    Update DatabaseRestore.CorrectOwner.sql

--- DatabaseRestore.CorrectOwner.sql
+++ DatabaseRestore.CorrectOwner.sql
@@ -2,9 +2,6 @@ IF NOT EXISTS(SELECT * FROM sys.objects o INNER JOIN sys.schemas s ON o.schema_i
 	EXEC( 'CREATE PROCEDURE [DatabaseRestore].[CorrectOwner] AS SELECT dt = GETDATE()' )
 GO
 
-USE [_ArtsDBAUtil]
-GO
-/****** Object:  StoredProcedure [DatabaseRestore].[CorrectOwner]    Script Date: 8/3/2017 10:18:44 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
